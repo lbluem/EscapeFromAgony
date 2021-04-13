705f3d307f19315222a74c8c0be9310e764b3b60
Entif <73485274+Entif@users.noreply.github.com>
2021-04-13 16:00:19 +0200

    Board :0

--- source/board.lua
+++ source/board.lua
@@ -1,3 +1,23 @@
 Hello
 My Name is Joe
 And I work in a button factory!
+
+playboard = {}
+
+absX = love.graphics.getWidth() 
+absY = love.graphics.getHeight()
+
+startX = absX * 0.06
+StartY = absY * 0.08
+
+tileX = absX * 0.144
+tileY = absY * 0.16
+
+function playboard:draw (dt)
+    love.graphics.setColor(1,1,1)
+    for i 1, 5 do
+        for j 1, 4 do 
+            love.graphics.rectangle("fill", startX, startY, tileX, tileY)
+        end
+    end
+end
